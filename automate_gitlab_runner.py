7e8f9f9840bf9c485d47905ae5fb077ca3d7ecdc
KL193 <lakshankasthuriarachchi@gmail.com>
2025-12-30 11:08:54 +0530

    Implement method to get installed version

--- automate_gitlab_runner.py
+++ automate_gitlab_runner.py
@@ -12,3 +12,14 @@ def run_command(cmd, check=True):
     if check and result.returncode != 0:
         raise Exception(f"Failed: {cmd}\nError: {result.stderr}")
     return result.stdout.strip()
+
+def get_installed_version():
+    try:
+        output = run_command("/usr/bin/gitlab-runner --version", check=False)
+        for line in output.splitlines():
+            if "Version:" in line:
+                return line.split()[1]
+        return None
+    except:
+        return None
+
