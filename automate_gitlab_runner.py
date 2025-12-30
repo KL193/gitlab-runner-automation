5553792e032c3b7fdb84af34b928a9c4cdf1859e
KL193 <lakshankasthuriarachchi@gmail.com>
2025-12-30 11:02:57 +0530

    Implement run_command() function

--- automate_gitlab_runner.py
+++ automate_gitlab_runner.py
@@ -5,3 +5,10 @@ import subprocess
 import time
 import urllib.parse
 import requests
+
+def run_command(cmd, check=True):
+    result = subprocess.run(cmd, shell=True, capture_output=True, text=True)
+    print(result.stdout)
+    if check and result.returncode != 0:
+        raise Exception(f"Failed: {cmd}\nError: {result.stderr}")
+    return result.stdout.strip()
